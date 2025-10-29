1b17adb47941e2b183c03b30d4499c9a718d0150
sebasorribes <sebastiansorribes@gmail.com>
2025-10-29 00:13:08 -0300

    Switch enemies to AnimatedSprite3D and update movement
    Replaced MeshInstance3D models for Ghost and Zombie with AnimatedSprite3D using new spritesheets and animations. Updated enemy scripts to play animations on ready. Adjusted enemy movement logic to use look_at in physics process, and increased player speed. Updated SoundTestScene to use BasicEnemy instead of Ghost, and removed SoundManager node.

--- Scripts/Enemies/Zombie.gd
+++ Scripts/Enemies/Zombie.gd
@@ -5,3 +5,4 @@ func _ready() -> void:
 	navAgent=$NavigationAgent3D
 	Detector = $Area3D
 	super._ready()
+	$AnimatedSprite3D.play("default")
--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-const SPEED = 3;
+const SPEED = 5;
 const DISTANCE_STEP = 0.9;
 
 var candyPoints = 0;
