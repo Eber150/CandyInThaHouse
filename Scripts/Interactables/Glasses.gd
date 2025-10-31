230abda7e4c7615b01560b675d8cbc352686ab69
Eber150 <ebernieto0@gmail.com>
2025-10-31 20:54:22 -0300

    masquenunca

--- Scripts/Interactables/Glasses.gd
+++ Scripts/Interactables/Glasses.gd
@@ -6,6 +6,9 @@ var ghosts :Array
 func _ready() -> void:
 	ghosts = get_tree().get_nodes_in_group("Ghosts")
 
+func _process(delta: float) -> void:
+	look_at(get_parent().get_node("player").position);
+
 func Interact() -> void:
 	for ghost in ghosts:
 		ghost.MakeVisible()
